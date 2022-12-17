de34d85808f74390a0522e50843c3f644a59c57e
mjlaurent6 <mjlaurent@connect.ust.hk>
2022-12-18 00:01:12 +0800

    setup project template ui & backend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,40 @@
 from typing import Union
+
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
 import uvicorn
 
-from fastapi import FastAPI
+
+fake_secret_token = "coneofsilence"
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Hello"}
+class Item(BaseModel):
+    id: str
+    title: str
+    description: Union[str, None] = None
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    fake_db[item.id] = item
+    return item
---
+++ ui/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Paperbase from "./components/Paperbase";
+
+function App() {
+  return <Paperbase/>
+}
+
+export default App;
